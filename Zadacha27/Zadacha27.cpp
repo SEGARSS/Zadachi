ba39eb0bf6484b1399d30afc5335916760841fb8
SEGARS <vim_branet@mail.ru>
2023-04-10 20:45:46 +0500

    27

---
+++ Zadacha27/Zadacha27.cpp
@@ -0,0 +1,26 @@
+/*
+Задача 27: 
+Напишите программу, 
+которая принимает на вход число и выдаёт сумму цифр в числе.
+*/
+
+#include <iostream>
+using namespace std;
+
+void main()
+{
+    setlocale(LC_ALL, "RU");
+    int n;
+    cout << "Введите число: ";
+    cin >> n;
+
+    int sum = 0;
+    while (n > 0) 
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+
+    cout << "Сумма цифр числа: " << sum << endl;
+}
+
