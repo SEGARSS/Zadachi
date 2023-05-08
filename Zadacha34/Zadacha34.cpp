65535f53fca6069366f1b343e074ebceaaae9646
SEGARS <vim_branet@mail.ru>
2023-05-08 20:43:40 +0500

    34 задача дорешали

--- Zadacha34/Zadacha34.cpp
+++ Zadacha34/Zadacha34.cpp
@@ -8,6 +8,7 @@
 */
 
 #include<iostream>
+#include <cstdlib>
 #include<ctime>
 using namespace std;
 
@@ -44,6 +45,40 @@ int countEven(int arr[], int SIZE)
 	return count;
 }
 
+void bubbleSort(int arr[], int size) {
+	// сортировка массива методом пузырька
+	for (int i = 0; i < size - 1; i++) 
+	{
+		for (int j = 0; j < size - i - 1; j++) 
+		{
+			if (arr[j] > arr[j + 1]) 
+			{
+				swap(arr[j], arr[j + 1]);
+			}
+		}
+	}
+}
+
+void countingSort(int arr[], int size) {
+	// сортировка массива методом подсчёта
+	const int MAX_VALUE = 999;
+	int count[MAX_VALUE + 1] = { 0 };
+
+	for (int i = 0; i < size; i++) 
+	{
+		count[arr[i]]++;
+	}
+
+	int index = 0;
+	for (int i = 0; i <= MAX_VALUE; i++) 
+	{
+		for (int j = 0; j < count[i]; j++) 
+		{
+			arr[index++] = i;
+		}
+	}
+}
+
 void main()
 {
 	setlocale(LC_ALL, "ru");
@@ -51,8 +86,19 @@ void main()
 
 	const int SIZE = 10;
 	int arr[SIZE];
+
 	FillArray(arr, SIZE);
+	cout << "Исходный массив:" << endl;
 	PrintArra(arr, SIZE);
+
+	int count = countEven(arr, SIZE);
+	cout << "Количество чётных чисел: " << count << endl;
+
+	cout << "Сортировка методом пузырька:" << endl;
+	bubbleSort(arr, SIZE);
+	PrintArra(arr, SIZE);
+
+	cout << "Сортировка методом подсчёта:" << endl;
 	countEven(arr, SIZE);
 	PrintArra(arr, SIZE);
 
