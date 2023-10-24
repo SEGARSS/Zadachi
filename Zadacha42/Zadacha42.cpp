d46a3968b14e6eeacef2efbcdffbe2862f78e983
SEGARS <vim_branet@mail.ru>
2023-10-24 18:41:56 +0500

    математические задачи

---
+++ Zadacha42/Zadacha42.cpp
@@ -0,0 +1,26 @@
+﻿//Задача №42
+// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+#include<iostream>
+#include<string>
+#include<bitset>
+using namespace std;
+
+int main ()
+{
+	setlocale(LC_ALL,"Ru");
+
+	int desyatichnot;
+	cout << "Введите десятичное число - ";
+	cin >> desyatichnot;
+
+	bitset<32> binaryNumber(desyatichnot);
+	cout << "Двоичное предствление - " << binaryNumber << endl;
+
+	return 0;
+}
+
+/*Задачка чисто математиматическая. Библиотека  #include<bitset> преобразует функцию десятичных чисел 
+в двоичные.*/
+
+
