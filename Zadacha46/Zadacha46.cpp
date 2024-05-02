2d62ee82719fe25bde8901acee13af6c19c3b1ed
SEGARSS <vim_branet@mail.ru>
2024-05-02 15:05:56 +0500

    задача на массив

--- Zadacha46/Zadacha46.cpp
+++ Zadacha46/Zadacha46.cpp
@@ -10,32 +10,32 @@ int main()
 	setlocale(LC_ALL,"ru");
 	srand(time(NULL));
 
-	int ROWS;
-	int COLS;
+	int rows;
+	int cols;
 
-	cout << "Введите количество строк ROWS: ";
-	cin >> ROWS;
-	cout << "Введите количество столбцов COLS: ";
-	cin >> COLS;
+	cout << "Введите количество строк rows: ";
+	cin >> rows;
+	cout << "Введите количество столбцов cols: ";
+	cin >> cols;
 
 	/*достаточно один раз уазать массиву (**), потом последующие указание не требуеться. */
-	int **arr = new int *[ROWS]; // Строки
+	int **arr = new int *[rows]; // Строки
 
-	for (int i = 0; i < ROWS; i++)
+	for (int i = 0; i < rows; i++)
 	{
-		arr[i] = new int[COLS]; // Столбцы
+		arr[i] = new int[cols]; // Столбцы
 
-		for (int j = 0; j < COLS; j++)
+		for (int j = 0; j < cols; j++)
 		{
 			arr[i][j] = rand() % 10;
 		}
 	}
 
-	cout << "Двумерный массив размером " << ROWS << "x" << COLS << " заполненный случайными числами:\n";
+	cout << "Двумерный массив размером " << rows << "x" << cols << " заполненный случайными числами:\n";
 
-	for (int i = 0; i < ROWS; i++)
+	for (int i = 0; i < rows; i++)
 	{
-		for (int j = 0; j < COLS; j++)
+		for (int j = 0; j < cols; j++)
 		{
 			cout << arr[i][j] << " ";
 		}
@@ -44,7 +44,7 @@ int main()
 
 	/*В данном случаи нет нужды указыват ьудаление arr[j], он и так был частью двух мерного массива.
 	После того как все строки были удалены, мы освобождаем память для массива указателей на строки с помощью delete[] arr;.*/
-	for (int i = 0; i < ROWS; i++) 
+	for (int i = 0; i < rows; i++) 
 	{
 		delete[] arr[i];
 	}
